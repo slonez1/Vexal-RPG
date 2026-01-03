300dcd61927805848411dcdfd51088429c9185e0
Slonez1 <bckraemer@gmail.com>
2026-01-03 01:47:05 +0000

    WIP: Saving changes before pulling remote updates

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 from fastapi import FastAPI, WebSocket, HTTPException
 from pydantic import BaseModel
 
 # Initialize app
+=======
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+import os
+
+>>>>>>> 9f02796 (Added backend folder with FastAPI app)
 app = FastAPI()
